69c5e9415a585772076329ca3abd02e5c788b6dc
xXYvesXx <yvesh20@gmail.com>
2020-10-21 17:36:22 -0300

    estilização e uso de uma api

---
+++ src/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home shadow-xl bg-gray-200 w-8/12 mx-auto p-20">
+    <h2 class="text-3xl text-blue-600 font-extrabold">Can I Haz Dad Jokes</h2>
+    <h3 class="h-40 text-back">{{ joke }}</h3>
+    <button
+      class="bg-blue-500 hover:bg-blue700 text-white font-bold py-2 px-4 rounded-full mt-10 focus:outline-none"
+      @click="clicked"
+    >
+      AddJoke
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    clicked() {
+      alert("Just clicked and waiting for the jokes");
+    },
+  },
+  data: () => ({}),
+  computed: {
+    joke() {
+      return this.$store.getters.getCurrentJoke;
+    },
+  },
+  /*
+  mounted() {
+    this.joke = this.$store.getters.getCurrentJoke;
+  },*/
+};
+</script>
+
+<style scoped>
+.home {
+  height: 400px;
+  max-height: 500px;
+}
+
+h3 {
+  font-size: 2.5rem;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,24 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+//Api for Jokes
+const url = 'https://icanhazdadjoke.com'
+const headers = { Accept: 'application/json' }
+
 export default new Vuex.Store({
   state: {
-    currentJoke: '',
-    allJokes: []
+    currentJoke: 'This is a joke',
+    allJokes: [],
   },
   mutations: {
-    setCurrentJoke(state,payload){
+    //syncronous
+    setCurrentJoke(state, payload) {
       state.currentJoke = payload
       state.allJokes.push(payload)
-    }
+    },
   },
   actions: {
+    //assyncronous
+    async setCurrentJoke(state) {
+      const joke = await fetch(url, { headers })
+      const j = await joke.json()
+      state.commit('setCurrentJoke', j.joke)
+    },
   },
-  modules: {
+  modules: {},
+  getters: {
+    getCurrentJoke: (state) => state.currentJoke,
   },
-  getters:{
-    getcurrentJoke(state){
-      return state.currentJoke
-    }
-  }
 })
